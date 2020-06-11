0d112c883ea54bfa45ca4d11b93f77eb3de5105c
mf014 <tanjing@mainfunc.cn>
2020-06-11 17:25:08 +0800

    [author] :tj [description] :mqtt  emq  demo01 版

---
+++ src/main/All.java
@@ -0,0 +1,26 @@
+package main;
+
+import frame.mqtt.MqttClientUtil;
+
+/**
+ * @author zx
+ */
+public class All {
+    public static void main(String[] args){
+        //mqtt服务启动
+        MqttClientUtil.createClient();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                try {
+                    //释放工作
+                    //...
+
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
